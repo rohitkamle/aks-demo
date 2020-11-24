9cb3fd18dd0a2bca4b7062b2cd4b29d5f5b8d69b
Rohit Kamle <Rohit.Kamle@silicus.com>
2020-11-24 12:22:59 +0530

    updated

--- tf-files/backend.tf
+++ tf-files/backend.tf
@@ -1,8 +1,5 @@
 terraform {   
  backend "azurerm" {
-     storage_account_name = "dephidemouaenorthsa"
-     container_name = "statedata"
-     key = "terraform.tfstate"
-     access_key = "MHaOg1opAt1S1E/wJ4L421sNy9OOFuKTEvvichiqFXTTXhMz5CljOoKl81/AOh/6+HeJQbhpqNQg6mSOBzABvQ=="
+
  } 
 }
